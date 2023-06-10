a141efe3bbf06dbe4e271aa46f13e6b052d8fd33
matiasvidela96 <matividela@yahoo.com.ar>
2023-06-10 10:19:25 -0300

    test-comentario

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 
 export default function ResetPassword() {
   return (
-    <section className="relative">
+    <section className="relative">// comentario
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
 
